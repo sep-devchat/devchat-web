7193c9a52f2c0f515c75d16f22b41c25823f7c9c
Ha Chang <hthhuyntrang@gmail.com>
2025-09-24 04:45:32 +0700

    feat(auth): fix UI for Confirm Mail page

--- src/components/ui/ConfirmBackground/ConfirmBg.styled.ts
+++ src/components/ui/ConfirmBackground/ConfirmBg.styled.ts
@@ -5,7 +5,14 @@ export const PageWrapper = styled.div`
   background: #fff;
   width: 100vw;
   z-index: -1;
-    position: absolute;
+  position: absolute;
+
+  @media (prefers-color-scheme: dark) {
+      background: linear-gradient(0deg, var(--primary-100, #0A1F43) 0%, var(--primary-100, #0A1F43) 100%), #FFF;
+              height: 100%;
+        top: 0px;
+        left: 0px;
+  }
 `;
 
 export const ElipYellow1 = styled.div`
@@ -20,6 +27,12 @@ export const ElipYellow1 = styled.div`
   position: fixed;
   top: 85px;
   left: -46px;
+
+  @media (prefers-color-scheme: dark) {
+    opacity: 0.58;
+    background: rgba(215, 215, 59, 0.52);
+    filter: blur(150px);
+  }
 `;
 
 export const ElipBlue1 = styled.div`
@@ -33,6 +46,12 @@ export const ElipBlue1 = styled.div`
   position: fixed;
   top: -71px;
   left: 249px;
+
+  @media (prefers-color-scheme: dark) {
+opacity: 0.58;
+background: rgba(0, 94, 255, 0.36);
+filter: blur(150px);
+  }
 `;
 
 export const ElipYellow2 = styled.div`
@@ -46,6 +65,12 @@ export const ElipYellow2 = styled.div`
   position: fixed;
   top: 358px;
   right: -22px;
+
+  @media (prefers-color-scheme: dark) {
+    opacity: 0.58;
+    background: rgba(215, 215, 59, 0.52);
+    filter: blur(150px);
+  }
 `;
 
 export const ElipBlue2 = styled.div`
@@ -60,4 +85,10 @@ export const ElipBlue2 = styled.div`
   position: fixed;
   bottom: -255px;
   left: 385px;
+
+   @media (prefers-color-scheme: dark) {
+opacity: 0.58;
+background: rgba(0, 94, 255, 0.36);
+filter: blur(150px);
+  }
 `;
--- src/pages/ConfirmMail/ConfirmMail.styled.ts
+++ src/pages/ConfirmMail/ConfirmMail.styled.ts
@@ -10,11 +10,27 @@ export const PageWrapper = styled.div`
 
 export const ContentCard = styled.div`
   display: flex;
-  padding: 0px 72px;
+  padding: 60px 100px;
   justify-content: center;
   align-items: center;
   gap: 30px;
+  border-radius: 30px;
   flex-direction: column;
+  background: #fff;
+  box-shadow: 2px 1px 56px 0 rgba(0, 0, 0, 0.11);
+
+  /* Dark mode styles */
+  @media (prefers-color-scheme: dark) {
+  border: 1px solid #FFF;
+    background: linear-gradient(
+      256deg,
+      rgba(255, 255, 255, 0.06) 0%,
+      rgba(153, 153, 153, 0.03) 100%
+    );
+    box-shadow: 2px 1px 56px 0 rgba(0, 0, 0, 0.11);
+    backdrop-filter: blur(7.650000095367432px);
+    -webkit-backdrop-filter: blur(7.650000095367432px);
+  }
 `;
 
 export const TitleGr = styled.div`
@@ -31,8 +47,11 @@ export const TitleCard = styled.h1`
   font-style: normal;
   font-weight: 800;
   line-height: normal;
-`;
 
+  @media (prefers-color-scheme: dark) {
+    color: #fff;
+  }
+`;
 
 export const SubmitButton = styled(Button)`
   width: 100%;
@@ -44,15 +63,19 @@ export const SubmitButton = styled(Button)`
     border: 1px solid ${theme.color.primary90};
     background: #fff;
   }
+
+  @media (prefers-color-scheme: dark) {
+    background: ${theme.color.primary};
+  }
 `;
 
 export const LoginButton = styled(Button)`
   width: 100%;
-  
+
   color: ${theme.color.primary90};
-    border: 1px solid ${theme.color.primary90};
-    background: #fff;
-  
+  border: 1px solid ${theme.color.primary90};
+  background: #fff;
+
   padding: 16px 0;
   &:hover {
     background: ${theme.color.primary90};
@@ -75,6 +98,10 @@ export const BackText = styled.p`
   &:hover {
     text-decoration: underline;
   }
+
+  @media (prefers-color-scheme: dark) {
+    color: #fff;
+  }
 `;
 
 export const Text = styled.p`
@@ -88,6 +115,10 @@ export const Text = styled.p`
   font-weight: 400;
   line-height: 100%; /* 16px */
   letter-spacing: 0.16px;
+
+  @media (prefers-color-scheme: dark) {
+    color: #fff;
+  }
 `;
 
 export const BoldText = styled.p`
@@ -101,6 +132,10 @@ export const BoldText = styled.p`
   font-weight: 700;
   line-height: 100%; /* 16px */
   letter-spacing: 0.16px;
+
+  @media (prefers-color-scheme: dark) {
+    color: #fff;
+  }
 `;
 
 export const ButtonActionGr = styled.div`
@@ -111,6 +146,6 @@ export const ButtonActionGr = styled.div`
 
 export const ConfirmImage = styled.img`
   width: 249px;
-height: 249px;
-aspect-ratio: 1/1;
+  height: 249px;
+  aspect-ratio: 1/1;
 `;
