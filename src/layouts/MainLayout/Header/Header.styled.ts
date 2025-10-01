2ffdaabcdb14c5ff8b6e8b861e766f1e4f16f6a1
unknown <tnnn0320@gmail.com>
2025-10-02 01:34:03 +0700

    Create feature add channel follow by new layout

--- src/layouts/MainLayout/Header/Header.styled.ts
+++ src/layouts/MainLayout/Header/Header.styled.ts
@@ -8,3 +8,15 @@ export const HeaderContainer = styled.header`
 	justify-content: space-between;
 	padding: 0 16px;
 `;
+
+export const NavTabTitle = styled.div`
+	display: flex;
+	flex-direction: row;
+	gap: 6px;
+	border: none;
+	margin-right: 16px;
+	font-size: 16px;
+	font-weight: 600;
+	color: #1e2a3b;
+	align-items: center;
+`;
